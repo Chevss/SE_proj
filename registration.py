0edc61598379b7b856cf694f1c4dc901855107a7
Chevss <chevy023.gaiti@gmail.com>
2024-06-28 15:31:30 +0800

    Update registration.py

--- registration.py
+++ registration.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from tkinter import Button, Canvas, Scrollbar, Entry, messagebox, OptionMenu, PhotoImage, Radiobutton, StringVar, Tk
 from tkcalendar import DateEntry
 from tkinter.ttk import Treeview
-
+import secrets
 # From user made modules
 import shared_state
 from salt_and_hash import generate_salt, hash_password
