7d5c9e1b0ae4db33f4128269d1b21846ef3e056b
Chevss <chevy023.gaiti@gmail.com>
2024-07-08 17:13:32 +0800

    fix for the executable

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import login
-
+import registration
+import database
+import sqlite3
 
 
 if __name__ == "__main__":
+    database.create_database()
+    conn = sqlite3.connect('Trimark_construction_supply.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM accounts WHERE Username = ?", ('admin',))
+
+    if not cursor.fetchone():
+        registration.save_user('admin', '', '', '', '', '', '', '', '', 'admin', 'admin', 0)
+    # save_user(loa, first_name, last_name, mi, suffix, birthdate, contact_number, home_address, email, username, password, is_void):
     login.create_login_window()
